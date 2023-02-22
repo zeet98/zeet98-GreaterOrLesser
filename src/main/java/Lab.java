8295adab42e457eb4d9619f6f9df235ee330b2d3
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-02-22 01:23:23 +0000

    Completed greater or assignment

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -10,6 +10,11 @@ public class Lab {
      *          If neither is true, return "Equal". This problem is case-sensitive!
      */
     public String decide(int a, int b){
+        if(a < b){
+            return "Lesser";
+        } else if (a > b){
+            return "Greater";
+        } else
         return "Equal";
     }
 }
